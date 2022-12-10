85eb45d1b2f7e503f4987f114167ca14d1d1c3ec
Dmitry Ivanov <diemon_2000@mail.ru>
2022-12-11 02:01:44 +0300

    Создан индекс-вывод пдф файлов персонажа, однако с помощью библиотеки syncfysion — необходимо найти бесплатный аналог.

--- BeyondCreator/Controllers/CharactersController.cs
+++ BeyondCreator/Controllers/CharactersController.cs
@@ -1,10 +1,4 @@
-﻿global using Syncfusion.Pdf.Parsing;
-using Syncfusion.Pdf;
-using Syncfusion.Pdf.Graphics;
-using PdfSharp.Pdf;
-using PdfSharp.Pdf.AcroForms;
-using Syncfusion.Drawing;
-using System.IO;
+﻿using IronPdf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -174,147 +168,19 @@ namespace BeyondCreator.Controllers
 
         //Создаем документ Сохранения PDF файла
 
-
-
+       
+        [HttpPost]
         //[Route("Characters/Details/{id?}")]
-        //[HttpPost]
-        public async Task<IActionResult> CreatePDFDocument(int? id)
+        public ActionResult CreatePDFDocument()
         {
-            //Проверка на null
-            if (id == null || _context.Character == null)
-            {
-                return NotFound();
-            }
+            var render = new IronPdf.ChromePdfRenderer();
+            render.RenderingOptions.PaperSize = IronPdf.Rendering.PdfPaperSize.A4;
+            render.RenderingOptions.ViewPortWidth= 1280;
+            var doc = render.RenderUrlAsPdf("https://metanit.com/sharp/aspnetmvc/2.2.php");
+            return File(doc.Stream.ToArray(), "application/pdf");
 
-            var character = await _context.Character
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (character == null)
-            {
-                return NotFound();
-            }
-
-            
-            //Указываем путь к файлу
-            FileStream docStream = new FileStream("C:\\Users\\user\\source\\repos\\BeyondCreator\\BeyondCreator\\wwwroot\\Files\\PDF\\CharacterSheet.pdf", FileMode.Open, FileAccess.Read);
-             PdfLoadedDocument loadedDocument = new PdfLoadedDocument(docStream);
-             //loading
-             PdfLoadedForm form = loadedDocument.Form;
-            //заполняем данные из файла
-            //1 — атрибуты
-            /* #region attrcheck
-              string str = " ";
-              string dex = " ";
-              string per = " ";
-              string cha = " ";
-              string int1 = " ";
-              string vit = " ";
-              if (character.s != null)
-              {
-                  NameCheck = character.Name.ToString();
-              }
-              if (character.Race != null)
-              {
-                  RaceCheck = character.Race.ToString();
-              }
-         #endregion */
-            
-
-
-            (form.Fields["CharStrenght"] as PdfLoadedTextBoxField).Text = character.Strenght.ToString();
-            (form.Fields["CharDexterity"] as PdfLoadedTextBoxField).Text = character.Dexterity.ToString() ?? " ";
-            (form.Fields["CharPerseption"] as PdfLoadedTextBoxField).Text = character.Perseption.ToString() ?? " " ;
-            (form.Fields["CharCharisma"] as PdfLoadedTextBoxField).Text = character.Charisma.ToString() ?? " ";
-            (form.Fields["CharInteligence"] as PdfLoadedTextBoxField).Text = character.Intellegence.ToString() ?? " ";
-            (form.Fields["CharVitality"] as PdfLoadedTextBoxField).Text = character.Vitality.ToString() ?? " ";
-            //Уровень и опыт
-            (form.Fields["CharLvl"] as PdfLoadedTextBoxField).Text = character.Lvl.ToString() ?? " ";
-            (form.Fields["CharExp"] as PdfLoadedTextBoxField).Text = character.Experience.ToString() ?? " ";
-            //Имя и раса
-            //Проверка имени и расы
-           /* #region nameracecheck
-            string NameCheck = " ";
-            string RaceCheck = " ";
-            if (character.Name != null)
-            {
-                NameCheck = character.Name.ToString();
-            }
-            if (character.Race != null)
-            {
-                RaceCheck = character.Race.ToString();
-            }
-            #endregion */
-            (form.Fields["CharRace"] as PdfLoadedTextBoxField).Text = character.Race.ToString();
-            (form.Fields["CharName"] as PdfLoadedTextBoxField).Text = character.Name.ToString();
-            //Воля
-            (form.Fields["CharWill"] as PdfLoadedTextBoxField).Text = character.Will.ToString() ?? " ";
-            //Доспех
-            (form.Fields["CharHeadArmour"] as PdfLoadedTextBoxField).Text = character.HeadArmour.ToString() ?? " ";
-            (form.Fields["CharBodyArmour"] as PdfLoadedTextBoxField).Text = character.BodyArmour.ToString() ?? " ";
-            (form.Fields["CharHandsArmour"] as PdfLoadedTextBoxField).Text = character.ArmsArmour.ToString() ?? " ";
-            (form.Fields["CharLegsArmour"] as PdfLoadedTextBoxField).Text = character.LegsArmour.ToString() ?? " ";
-            //Преимущества
-
-            //Проверка преимущества
-            #region advantages check
-            /* string adv1 = " ";
-             if (character.Advantage1 != null)
-             {
-                 adv1 = character.Advantage1.ToString();
-             }
-             string adv2 = " ";
-             if (character.Advantage2 != null)
-             {
-                 adv2 = character.Advantage2.ToString();
-             }
-             string adv3 = " ";
-             if (character.Advantage3 != null)
-             {
-                 adv3 = character.Advantage3.ToString();
-             }*/
-             #endregion 
-            (form.Fields["CharAdvantage1"] as PdfLoadedTextBoxField).Text = character.Advantage1.ToString();
-            (form.Fields["CharAdvantage2"] as PdfLoadedTextBoxField).Text = character.Advantage2.ToString();
-            (form.Fields["CharAdvantage3"] as PdfLoadedTextBoxField).Text = character.Advantage3.ToString();
-            //Недостатки
-            //Проверка недостатков
-            /* #region disadvantages check
-             string dis1 = " ";
-             if (character.Disadvantage1 != null)
-             {
-                 dis1 = character.Advantage1.ToString();
-             }
-             string dis2 = " ";
-             if (character.Disadvantage2 != null)
-             {
-                 dis2 = character.Advantage2.ToString();
-             }
-             string dis3 = " ";
-             if (character.Disadvantage3 != null)
-             {
-                 dis3 = character.Advantage3.ToString();
-             }
-             #endregion */
-            (form.Fields["CharDisadvantage1"] as PdfLoadedTextBoxField).Text = character.Disadvantage1.ToString();
-            (form.Fields["CharDisadvantage2"] as PdfLoadedTextBoxField).Text = character.Disadvantage2.ToString();
-            (form.Fields["CharDisadvantage3"] as PdfLoadedTextBoxField).Text = character.Disadvantage3.ToString();
-            //профессии
-            (form.Fields["CharProfession1"] as PdfLoadedTextBoxField).Text =character.Profession1.ToString();
-            (form.Fields["CharProfession2"] as PdfLoadedTextBoxField).Text =character.Profession2.ToString();
-            (form.Fields["CharProfession3"] as PdfLoadedTextBoxField).Text =character.Profession3.ToString();
-            //Пишем документ в поток
-            MemoryStream stream = new MemoryStream();
-              loadedDocument.Save(stream);
-              //Ставим позицию потока в 0
-              stream.Position = 0;
-              //Закрываем документ
-              loadedDocument.Close(true);
-              //Определяем тип контента для файла
-              string contentType = "application/pdf";
-              //Определяем имя файла
-              string filename = "Character.pdf";
-              //создаём FileContentResult
-              return File(stream, contentType, filename); 
         }
+
 
     }
 }
