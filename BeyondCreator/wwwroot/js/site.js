ba1d2b784c0a7ede3c47d96d58c2db40f1e6306b
Dmitry Ivanov <diemon_2000@mail.ru>
2023-01-23 00:11:59 +0300

    Изменён layout файл, добавлен js триггер, позволяющий обращаться к навигационному меню с помощью экрана с небольшим разрешением

--- BeyondCreator/wwwroot/js/site.js
+++ BeyondCreator/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function open_navcol_1() {
+    var x = document.getElementById("navcol-1");
+    if (x.style.display === "none") {
+        x.style.display = "block"
+    }
+    else {
+        x.style.display = "none"
+    }
+}
